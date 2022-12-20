19b6c846bb18f8e75076ca24f27448296dff008a
Trevor <thomasjamestrevor@gmail.com>
2022-12-20 11:12:40 -0600

    Move FATFS and add Middlewares

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -75,6 +75,8 @@ void Error_Handler(void);
 #define SMPS_SW_GPIO_Port GPIOA
 #define LD4_Pin GPIO_PIN_13
 #define LD4_GPIO_Port GPIOB
+#define SD_CS_Pin GPIO_PIN_8
+#define SD_CS_GPIO_Port GPIOC
 #define AX_CS_Pin GPIO_PIN_10
 #define AX_CS_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
@@ -83,6 +85,7 @@ void Error_Handler(void);
 #define TCK_GPIO_Port GPIOA
 #define SWO_Pin GPIO_PIN_3
 #define SWO_GPIO_Port GPIOB
+
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
